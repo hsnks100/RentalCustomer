e8d256a355dc3fb3c1cb326d5a282a5d444da81f
knotebook <hsnks100@gmail.com>
2017-04-25 15:24:40 +0900

    FrequentPoints 까지

--- src/Movie.java
+++ src/Movie.java
@@ -41,8 +41,6 @@ public class Movie {
 		return _price.getCharge(daysRented);
 	};
 	public int getFrequentRenterPoints(int daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-			return 2;
-		return 1;
+		return _price.getFrequentRenterPoints(daysRented);
 	}
 }
--- src/Price.java
+++ src/Price.java
@@ -3,6 +3,9 @@ public abstract class Price {
 	abstract int getPriceCode();
 
 	abstract double getCharge(int daysRented);
+	public int getFrequentRenterPoints(int daysRented) {
+		return 1;
+	}
 }
 
 class RegularPrice extends Price {
@@ -42,5 +45,10 @@ class NewReleasePrice extends Price {
 		double result = daysRented * 3;
 		return result;
 
+	}
+	public int getFrequentRenterPoints(int daysRented) {
+		if (daysRented > 1)
+			return 2;
+		return 1;
 	}
 }
