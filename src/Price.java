e0595a1f5314764a0471c2bffe8f28c3b44f96f8
knotebook <hsnks100@gmail.com>
2017-04-25 15:18:49 +0900

    poly

---
+++ src/Price.java
@@ -0,0 +1,46 @@
+
+public abstract class Price {
+	abstract int getPriceCode();
+
+	abstract double getCharge(int daysRented);
+}
+
+class RegularPrice extends Price {
+	int getPriceCode() {
+		return Movie.REGULAR;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2)
+			result += (daysRented - 2) * 1.5;
+		return result;
+
+	}
+}
+
+class ChildrenPrice extends Price {
+	int getPriceCode() {
+		return Movie.CHILDRENS;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3)
+			result += (daysRented - 3) * 1.5;
+		return result;
+
+	}
+}
+
+class NewReleasePrice extends Price {
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = daysRented * 3;
+		return result;
+
+	}
+}
